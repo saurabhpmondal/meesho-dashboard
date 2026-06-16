ba6778a985cce14b9dedec34f7665d3303477fc3
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-16 11:32:59 +0530

    Update tabsConfig.js

--- js/config/tabsConfig.js
+++ js/config/tabsConfig.js
@@ -1,13 +1,20 @@
-export const TABS = [
-
-    {
-        id: 'dashboard',
-        label: 'Dashboard'
-    },
-
-    {
-        id: 'topStyles',
-        label: 'Top Styles'
-    }
-
+/**
+ * Strict structural configuration for application tabs.
+ * To add new tabs moving forward, add a new configuration block here.
+ */
+export const TABS_CONFIG = [
+  {
+    id: 'dashboard',
+    label: 'Dashboard',
+    reportClass: 'DashboardReport',
+    icon: 'fa-th-large'
+  },
+  {
+    id: 'topStyles',
+    label: 'Top Styles',
+    reportClass: 'TopStylesReport',
+    icon: 'fa-tags'
+  }
 ];
+
+export const DEFAULT_TAB_ID = 'dashboard';
